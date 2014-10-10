bbf0d9b7297a838847fb0d644353da9ccd3f4555
Edward <eschemb1@jhu.edu>
2014-10-10 00:41:27 -0400

    Update pseudo.c

--- pseudo.c
+++ pseudo.c
@@ -1,13 +1,34 @@
+/**
+ * TODO: Remove isFinished from token.h
+ *       Come up with ending logic
+ *       Last lowered?
+ */
+
 for(;;) {
 
 	if(has_token) {
 	
 		/*Logic for updating the token*/
+		if((token->aru > local_aru) || (/*You were last one to lower*/)) {
+			token->aru = local_aru;
+		}
 
 		/*If you have any packets in the retrans in your packet holding
 		 * array, send them*/
+		for(i = 0; i < RETRANS_SIZE; i++) {
+			if(frame[i%FRAME_SIZE]->packet_index == token->retransmission_request[i]) {
+				sendto( ss, frame[i%FRAME_SIZE], sizeof(packet), 0, (struct sockaddr *)&send_addr, 
+					sizeof(send_addr) );
+			}
+		}
+		
 
 		/*Add missing packets to the rtr array in the token*/
+		for(i = 0; i < FRAME_SIZE; i++) {
+			if(frame[i]->packet_index < last_written) {
+				/*Add to rtr if not already in it*/
+			}
+		}
 
 		/*If sent_packets < num_packets*/
 
@@ -21,11 +42,22 @@ for(;;) {
 				/*FINISHING LOGIC*/
 
 		/*Multicast all packets in your frame*/
+		/*for all packets in frame: */
+			/*Multicast packet*/
+			if((token->aru == token->sequence) && (local_aru == token->aru) ) {
+				token->aru++;
+			}
+			if(packet->index > token->sequence) {
+				token_sequence = packet->index;
+			}
+		
+		/*Update token's seq*/
 
 		/*Set last_seen_ARU to the token's aru*/
 
 		/*Send the token to your neighbor*/
-		/*When you get ack, has_token = 0*/
+		/*Wait for ack*/
+		/*has_token = 0*/
 
 	}else{
 		/*If Receive type=0, you got data packet */
